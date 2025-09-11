d0ea3ecf9f5949a6e0aff0f3e4fbc3177c924832
JOY-MACWAN <2439mca.joy@aimis.ac.in>
2025-09-11 09:06:47 +0000

    Your commit message here

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use((req, res) => {
 });
 
 // ================= Start Server =================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
